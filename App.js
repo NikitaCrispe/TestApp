d2926cf12fc45d50e23a079feb0691ca54464a70
Nikita Crispe <nikitacrispe@gmail.com>
2024-01-29 12:27:45 -0500

    built a button that increments the count of our number

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(1); // Initial count value is 1
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My first react native app. Yay!</Text>
+      <Button title="Count" onPress={incrementCount} />
+      <Text>{count}</Text>
       <StatusBar style="auto" />
     </View>
   );
